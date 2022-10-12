c9b8834549396bc7b4fd7ae9307c012ab9a8847c
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2022-10-12 20:56:17 +0300

    Add custom User Model

---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+class User {
+
+  final String uid;
+
+  User({required this.uid});
+
+}
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,20 @@
+import 'package:coffee_app/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? _userFromFirebaseUser(FirebaseUser user){
+    return user !=  null ? User(uid: user.uid) : null;
+  }
+
+
   Future SignInAnon () async {
     try{
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+     return _userFromFirebaseUser(user);
     }
     catch(e){
       print(e.toString());
