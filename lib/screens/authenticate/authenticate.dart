2ce7f3a7b7e842825fc7950af4c4e488f186af08
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2022-10-12 12:33:12 +0300

    Basic app structure

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  const Authenticate({Key? key}) : super(key: key);
+
+  @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('authenticate'),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:coffee_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
