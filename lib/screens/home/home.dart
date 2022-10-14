8c88103745e473189f6474cfb32b863e12b4f3c2
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2022-10-14 17:16:17 +0300

    Sign out implementation

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,37 @@
+import 'package:coffee_app/services/auth.dart';
 import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final AuthService _auth = AuthService();
     return Container(
-      child: Text('Home'),
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Coffee App'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0,
+          actions: [
+            TextButton.icon(
+              onPressed: () async{
+                await _auth.signOut();
+              },
+              icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+              ),
+              label: Text(
+                  'logout',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -24,4 +24,14 @@ class AuthService{
       return null;
     }
   }
+
+  Future signOut() async {
+    try{
+      return await _auth.signOut();
+    }
+    catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
 }
